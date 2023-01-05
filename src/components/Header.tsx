7db584cc62593717e8fdd6f7748813ce0d616575
vitordiniz25 <vitordiniz25@gmail.com>
2023-01-05 09:34:00 -0300

    finalizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -43,6 +47,34 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20"></Icon>
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.300"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Vitor Diniz</Text>
+            <Text color="gray.300">vitordiniz25@gmail.com</Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Vitor Diniz"
+            src="https://github.com/vitordiniz25.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
