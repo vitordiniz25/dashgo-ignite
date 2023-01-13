1c46abd9bd2abc43ef02d38a52b1a31a93b19ee2
vitordiniz25 <vitordiniz25@gmail.com>
2023-01-13 18:14:48 -0300

    sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -31,7 +31,7 @@ type User = {
 };
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -60,6 +60,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
